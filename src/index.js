2a6fa97e91f033bd7725543719699a76ed4ac2f5
kanehiro <kanehiro7575@gmail.com>
2022-02-22 16:00:02 +0900

    B2

--- src/index.js
+++ src/index.js
@@ -129,7 +129,9 @@
 
 	/* 演習1 ここから */
 	function add(){}
-	function sub(){}
+	function sub(){
+		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
+	}
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
